7459fc86d1a230137af36cd183cede434a7ff23c
keith-rhd <keith@615house.com>
2025-12-15 20:00:40 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 # config.py
 from dataclasses import dataclass
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent
-GEOJSON_LOCAL_PATH = BASE_DIR / "tn_counties.geojson"
 
 SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTw_-UeODGJQFKDMVXM59CG45SrbADPQpyWcALENIDqT8SUhHFm1URYNP3aB6vudjzpM1mBFRio3rWi/pub?output=csv"
 
